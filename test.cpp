b08e94a65f6af5121e299c6c15edca092b56b629
Nguyen Tat Dat <datnguyn1104@gmail.com>
2024-05-24 09:06:58 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main (){
     cout << "This is the first time I use github" << endl;
-    cout << "Co biet dung khong?" << endl;
+    cout << "Can you push or pull?" << endl;
+    cout << "It so easy" << endl;
     return 0;
 }
