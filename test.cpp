4821454a7a58a3d329c07b260403e40d06a2376d
Nguyen Tat Dat <datnguyn1104@gmail.com>
2024-05-24 09:01:39 +0700

    test git pull test.cpp

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main (){
-    cout << "This is the first time I use github";
+    cout << "This is the first time I use github" << endl;
+    cout << "Co biet dung khong?" << endl;
     return 0;
 }
